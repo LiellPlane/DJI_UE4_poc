3bfd7bc13001d2eec9fe82dcb0fb5dfc53c7a633
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-02-27 21:43:43 +0000

    Update lumoscript.py

--- Source/lumotag/lumoscript.py
+++ Source/lumotag/lumoscript.py
@@ -67,9 +67,9 @@ class libcam_commands(enum.Enum):
 
 
 class HQ_Cam_vidmodes(enum.Enum):
+    _4 = ["640 × 480",(640, 480)] #0.3MP
     _2 = ["2028 × 1080p50,",(2020, 1080)] # 2.0MP  this is not losing res -  turn camera 90 degrees - probably want this one
     _3 = ["1332 × 990p120",(1332, 990)] 
-    _4 = ["640 × 480",(640, 480)] #0.3MP
     _1 = ["2028 × 1520p40",(2020, 1520)]
 
 
@@ -453,7 +453,6 @@ def decode_pattern(lumostate : lumogun_state):
                 
                 (x, y) = lumostate.long_vid_res#  need to do this for YUV!
                 output = output[0:y, 0:x]#  need to do this for YUV!
-                output = cv2.resize(output,(480, 640))
                 if output is None:
                     continue
                 #try:
@@ -507,7 +506,6 @@ def decode_pattern(lumostate : lumogun_state):
                 
                 (x, y) = lumostate.long_vid_res#  Need to do this for YUV!
                 output = output[0:y, 0:x]#  Need to do this for YUV!
-                output = cv2.resize(output,(480, 640))
                 print("image capture time")
             with decode_clothID.time_it():
                 
