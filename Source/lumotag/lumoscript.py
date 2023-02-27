49d876d7ae824f7d1145624e3be2b7d89ff68f9f
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-02-27 21:31:35 +0000

    Update lumoscript.py

--- Source/lumotag/lumoscript.py
+++ Source/lumotag/lumoscript.py
@@ -67,8 +67,9 @@ class libcam_commands(enum.Enum):
 
 
 class HQ_Cam_vidmodes(enum.Enum):
-    _4 = ["640 × 480",(640, 480)] #0.3MP
     _3 = ["1332 × 990p120",(1332, 990)] 
+    _4 = ["640 × 480",(640, 480)] #0.3MP
+
     _2 = ["2028 × 1080p50,",(2020, 1080)] # 2.0MP  this is not losing res -  turn camera 90 degrees - probably want this one
     _1 = ["2028 × 1520p40",(2020, 1520)]
 
