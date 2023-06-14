b406110574d641c77420e5ba3b927ad776c758ce
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-06-14 19:50:27 +0100

    Update raspberry_hardware.py

--- Source/lumotag/raspberry_hardware.py
+++ Source/lumotag/raspberry_hardware.py
@@ -102,9 +102,10 @@ class display(factory.display):
 
         output = cv2.resize(output, self.screen_size)
 
-        output = cv2.normalize(output, output,0, 255, cv2.NORM_MINMAX)
+        #output = cv2.normalize(output, output,0, 255, cv2.NORM_MINMAX)
 
         output = cv2.applyColorMap(output, cv2.COLORMAP_JET)
+
         lumo_viewer(output,0,False,False)
 
 
