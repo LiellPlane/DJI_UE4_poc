bce9e6e454b53751857fbada8803c78dc794d260
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-08-10 22:44:04 +0100

    Update async_cam_lib.py

--- Source/scambilight/async_cam_lib.py
+++ Source/scambilight/async_cam_lib.py
@@ -317,7 +317,8 @@ class ScambilightCamImageGen(ImageGenerator):
         self.picam2.configure(_config)
         #  set_controls must come after config!!
         #self.picam2.set_controls({"AnalogueGain": 10.0})
-        self.picam2.set_controls({"ExposureTime": 100000000000000}) # for blurring - but can get over exposed at night
+        #self.picam2.set_controls({"ExposureTime": 100000000000000}) # for blurring - but can get over exposed at night
+        self.picam2.controls.ExposureTime = 100000
         #self.picam2.set_controls({"FrameDurationLimits": (1000,1000)})
         self.picam2.start()
         time.sleep(0.2)
