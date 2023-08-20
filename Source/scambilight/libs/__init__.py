e893452ee17167d208892e4f3f494f4fde6c10c3
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-08-20 18:20:26 +0100

    Update __init__.py

--- Source/scambilight/libs/__init__.py
+++ Source/scambilight/libs/__init__.py
@@ -7,5 +7,5 @@ import imp
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 #sys.path.append(str(Path(parent).resolve().parents[0]) + "\\lumotag\\factory.py")
-module_name = imp.load_source('factory', str(Path(parent).resolve().parents[0]) + "\\lumotag\\factory.py")
-module_name = imp.load_source('img_processing', str(Path(parent).resolve().parents[0]) + "\\lumotag\\img_processing.py")
+module_name = imp.load_source('factory', str(Path(parent).resolve().parents[0]) + "/lumotag/factory.py")
+module_name = imp.load_source('img_processing', str(Path(parent).resolve().parents[0]) + "/lumotag/img_processing.py")
