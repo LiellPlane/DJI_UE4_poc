afecccfd7d66fc5b326029501d642828b1244d61
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-08-24 21:40:11 +0100

    Update raspberry_hardware.py

--- Source/lumotag/raspberry_hardware.py
+++ Source/lumotag/raspberry_hardware.py
@@ -282,7 +282,7 @@ class Relay(factory.Relay):
         return True
 
 
-class CsiCameraImageGen(factory.ImageGenerator):
+class CsiCameraImageGen_GS(factory.ImageGenerator):
     
     def __init__(self, res) -> None:
         self.cam_res = tuple(reversed(res))
@@ -304,11 +304,38 @@ class CsiCameraImageGen(factory.ImageGenerator):
         print("get_image", output.shape, output.dtype)
         return output
 
+
+class CsiCameraImageGen_HQ(factory.ImageGenerator):
+    
+    def __init__(self, res) -> None:
+        self.cam_res = tuple(reversed(res))
+        self.picam2 = Picamera2()
+        _config = self.picam2.create_video_configuration(
+                    main={"size": res,  "format": "YUV420"})#, controls={"FrameDurationLimits": (233333, 233333)})
+                #self.picam2.set_controls({"ExposureTime": 1000}) # for blurring - but can get over exposed at night
+        self.picam2.configure(_config)
+        #  set_controls must come after config!!
+        self.picam2.set_controls({"AnalogueGain": 10.0})
+        self.picam2.start()
+        time.sleep(0.2)
+
+    def get_image(self):
+        output = self.picam2.capture_array("main")
+        x = self.cam_res[1]
+        y = self.cam_res[0]
+        output = output[0: y, 0: x]#  Need to do this for YUV!
+        print("get_image", output.shape, output.dtype)
+        return output
+
 class CSI_Camera_Async(factory.CameraAsync):
     
     def __init__(self, video_modes) -> None:
-        super().__init__(video_modes, CsiCameraImageGen)
-
+        if video_modes == factory.HQ_Cam_vidmodes:
+            super().__init__(video_modes, CsiCameraImageGen_HQ)
+        elif video_modes == factory.HQ_GS_Cam_vidmodes:
+            super().__init__(video_modes, CsiCameraImageGen_GS)
+        else:
+            raise Exception("no match for video mode input")
 
 class CSI_Camera(factory.Camera):
 
