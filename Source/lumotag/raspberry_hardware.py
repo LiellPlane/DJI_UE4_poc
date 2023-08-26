8d044a20cdc22d4cb5f86777f8f6eb58655eeb5d
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-08-26 20:55:49 +0100

    Update raspberry_hardware.py

--- Source/lumotag/raspberry_hardware.py
+++ Source/lumotag/raspberry_hardware.py
@@ -307,7 +307,7 @@ class CsiCameraImageGen_GS(factory.ImageGenerator):
 class CsiCameraImageGen_GS_test(factory.ImageGenerator):
     
     def __init__(self, res) -> None:
-        self.cam_res = tuple(reversed(res))
+        self.cam_res = res
         self.picam2 = Picamera2()
         _config = self.picam2.create_video_configuration(
                     main={"size": res,  "format": "YUV420"})#, controls={"FrameDurationLimits": (233333, 233333)})
