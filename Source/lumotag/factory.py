0c52a26e8964bfd6dc1ed0f1340744f331d152cf
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-08-26 21:15:23 +0100

    ggv

--- Source/lumotag/factory.py
+++ Source/lumotag/factory.py
@@ -36,8 +36,7 @@ class HQ_Cam_vidmodes(Enum):
 
 class HQ_GS_Cam_vidmodes(Enum):
     """global shutter model"""
-    # in W/H format due to quirk of sensor
-    _2 = ["1456 × 1088p50,",(1088, 1456)]
+    _2 = ["1456 × 1088p50,",(1456, 1088)]
 
 
 class Fake_Cam_vidmodes(Enum):
