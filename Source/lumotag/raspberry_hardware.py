039939147c63210ff0b9371388556425f572c7d4
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-09-04 12:35:00 +0100

    Update raspberry_hardware.py

--- Source/lumotag/raspberry_hardware.py
+++ Source/lumotag/raspberry_hardware.py
@@ -64,7 +64,7 @@ def lumo_viewer(
         cv2.imshow("img", inputimage)
         #move_windowx = random.randint(0, 500)
         move_windowx = 0
-        move_windowy = random.randint(0, 500)
+        move_windowy = 250#random.randint(0, 500)
         if move_windowx is not None and move_windowy is not None:
             cv2.moveWindow("img", move_windowx, move_windowy)
         if presskey==True:
