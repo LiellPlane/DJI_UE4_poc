54561015803082211cbe62141cf8b8d71a2991f3
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2023-09-10 11:18:07 +0100

    tidy up

--- Source/IVILO_2_24/IVILO_2_24.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "IVILO_2_24.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IVILO_2_24, "IVILO_2_24" );
