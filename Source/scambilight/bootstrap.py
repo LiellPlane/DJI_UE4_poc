9ded90abe036f11b1310e9c8e8c7ccaead09128b
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-01-27 23:09:02 +0000

    pathetic

--- Source/scambilight/bootstrap.py
+++ Source/scambilight/bootstrap.py
@@ -7,14 +7,14 @@ import sys
 repo = 'https://github.com/LiellPlane/DJI_UE4_poc.git'
 codepath = "/home/scambilight/scambilight"
 if os.path.exists(codepath):
-    fetch_result = subprocess.run(['git', 'fetch'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    fetch_result = subprocess.run(['sudo', 'git', 'fetch'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(fetch_result)
-    fetch_result = subprocess.run(['git', 'pull', '--ff-only'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    fetch_result = subprocess.run(['sudo', 'git', 'pull', '--ff-only'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(fetch_result)
 else:
     print(
         "trying to clone repo from web - this is impossible state- should exist already")
-    subprocess.run(['git', 'clone', repo])
+    subprocess.run(['sudo', 'git', 'clone', repo])
 
 sys.path.append(os.path.abspath(f"{codepath}/Source/scambilight/"))
 
