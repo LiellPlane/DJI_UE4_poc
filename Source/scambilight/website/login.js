a795f5891238181b63d92927c8e648cbc543a93d
liell-opti <liell_p@optimove.com>
2024-03-06 13:03:39 +0000

    BROKEN DANGERgit add ./

--- Source/scambilight/website/login.js
+++ Source/scambilight/website/login.js
@@ -1,6 +1,8 @@
+let targetUrl   = 'https://api.scambilight.com/hello';
 function submitForm() {
     // Get form data
-    const targetUrl   = 'https://api.scambilight.com/hello';
+    
+    
     var form = document.getElementById("passwordform");
     var formData = new FormData(form);
     let valuesValid = true;
@@ -23,22 +25,44 @@ function submitForm() {
     // Nest the JSON object under "login" key
     let requestData = { "login": jsonData };
 
-    fetch(targetUrl, {
+        fetch(targetUrl, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify(requestData)
+        }).then(response => response.json())
+            .then(json => {console.log(JSON.stringify(json));
+            //alert(JSON.stringify(json));
+            // Check if "sessionbody" key exists in the JSON response
+            if (json.hasOwnProperty("sessiontoken")) {
+                // Store the value of "sessionbody" in sessionStorage
+                sessionStorage.setItem("sessiontoken", JSON.stringify(json.sessiontoken));
+                window.location.href = 'index.html';
+            }
+            else
+            {
+                alert(JSON.stringify(json));
+            }
+            });
+    }
+    window.onload = test_logged_in = () => {
+        console.log("testing if logged in")
+        let data = {
+            //"authentication": inputPassword,
+            "action": "NONE",
+        };
+        fetch(targetUrl, {
         method: "POST",
         headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(requestData)
-    }).then(response => response.json())
-        .then(json => {console.log(JSON.stringify(json));
-        //alert(JSON.stringify(json));
-        // Check if "sessionbody" key exists in the JSON response
-        if (json.hasOwnProperty("sessiontoken")) {
-            // Store the value of "sessionbody" in sessionStorage
-            sessionStorage.setItem("sessiontoken", JSON.stringify(json.sessiontoken));
-            window.location.href = 'index.html';
-        }
-        else
-        {
-            alert(JSON.stringify(json));
-        }
-        });
+        body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(json => {
+                console.log(JSON.stringify(json));
+                if (json.success && json.message !== "stranger danger") { 
+                    window.location.href = 'index.html'; // Replace 'success_page.html' with the actual URL of the page you want to redirect to
+                } else {
+                    alert(JSON.stringify(json));
+                }
+            });
+    
+        return true;
     }
