60319c941672b01cf3e6114765713d1223b2fef6
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-03-27 18:54:00 +0000

    plop

--- Source/scambilight/update_repo.sh
+++ Source/scambilight/update_repo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# might need chmod +x your_script.sh to set permissions
+# then use ./blahblah to launch in current shell
 cd /home/scambilight/DJI_UE4_poc
 git fetch
 git pull --ff-only
+cd /home/scambilight/
