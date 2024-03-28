7924def92b6c3bbb2288d6c70fba5364e1fa198f
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-03-28 20:38:18 +0000

    plop

--- Source/scambilight/update_repo.sh
+++ Source/scambilight/update_repo.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-# might need chmod +x your_script.sh to set permissions
-# then use ./blahblah to launch in current shell
-#rm /home/scambilight/*.cunt
-cd /home/scambilight/DJI_UE4_poc
-local_file="/home/scambilight/fuckfetch.cunt"
-git fetch > "$local_file" 2>&1
-local_file="/home/scambilight/retardedlinuxgitpull.cunt"
-git pull --ff-only > "$local_file" 2>&1
+
+# bullshit cloning "dubious folder" giving me a lot of problems, so use this same file to clone and pull the repo so it
+#can't be retarded about ownership 
+
+pull_log="/home/scambilight/retardedlinuxgitpull.cunt"
+clone_log="/home/scambilight/retardedlinuxgitclone.cunt"
+
+folder="/home/scambilight/DJI_UE4_poc"
+
+
+# Function to check if internet connection is available
+check_internet() {
+    ping -c 1 google.com > /dev/null 2>&1
+}
+
+# Wait for internet connection
+echo "Waiting for internet connection..."
+while ! check_internet; do
+    sleep 10  # Adjust the sleep duration as needed
+done
+echo "Internet connection established."
+
+if [ -d "$folder" ]; then
+    cd /home/scambilight/DJI_UE4_poc
+    git pull --ff-only > "$pull_log" 2>&1
+else
+    cd /home/scambilight
+    git clone https://github.com/LiellPlane/DJI_UE4_poc.git > "$clone_log" 2>&1
+fi
+
+
 cd /home/scambilight/
