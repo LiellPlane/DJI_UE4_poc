8c0c92845438c4a9795ea780714d17950c6172cb
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-03-31 21:00:29 +0100

    plop

--- Source/scambilight/testing/test_send_LED_deets.py
+++ Source/scambilight/testing/test_send_LED_deets.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
 
     senddic_list = []
 
+
+
     single_Led_size_bytes = sys.getsizeof(json.dumps({300:(255,255,255)}))
     udp_payload_bytes = 700
     leds_per_packet = udp_payload_bytes // single_Led_size_bytes
