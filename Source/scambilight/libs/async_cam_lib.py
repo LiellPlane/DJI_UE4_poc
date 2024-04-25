58083cc8a946c56fc8014d4093ff44feb44052e3
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-04-25 19:00:28 +0100

    plop

--- Source/scambilight/libs/async_cam_lib.py
+++ Source/scambilight/libs/async_cam_lib.py
@@ -199,7 +199,7 @@ class ScambilightCamImageGen(ImageGenerator):
         # have to reverse as quirk of ov5647 camera
         res_xy = tuple(reversed(res[0:2]))
         _config = self.picam2.create_video_configuration(
-                    main={"size": res_xy, "format": "RGB888"}, controls={'FrameRate': 50})#, controls={"FrameDurationLimits": (233333, 233333)})
+                    main={"size": res_xy, "format": "RGB888"}, controls={'FrameRate': 90})#, controls={"FrameDurationLimits": (233333, 233333)})
         self.picam2.configure(_config)
         #  set_controls must come after config!!
         self.picam2.set_controls({"AwbEnable": 0})
