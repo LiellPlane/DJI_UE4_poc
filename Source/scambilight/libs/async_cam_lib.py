1665275f32bd010be4bdb3c3b73a3ee17624c321
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-07-20 22:29:23 +0100

    plop

--- Source/scambilight/libs/async_cam_lib.py
+++ Source/scambilight/libs/async_cam_lib.py
@@ -215,20 +215,20 @@ class ScambilightCamImageGen_fps_test(ImageGenerator):
         self.picam2 = Picamera2()
         # have to reverse as quirk of ov5647 camera
         res_xy = tuple(reversed(res[0:2]))
-        _config = self.picam2.create_video_configuration(
-                    main={"size": res_xy, "format": "RGB888"},
-                    controls={ "FrameDurationLimits": (22222, 33333)}, # ex FrameDurationLimits:  24fps = 1000000/24 = 41667
-                    buffer_count=1)#, controls={"FrameDurationLimits": (22222, 33333)})
-        self.picam2.configure(_config)
-        #  set_controls must come after config!!
-        self.picam2.set_controls({"AwbEnable": 0})
-        #self.picam2.set_controls({"AeEnable": 0})
-        self.picam2.set_controls({"AeMeteringMode": controls.AeMeteringModeEnum.Spot})
-        self.picam2.set_controls({"AnalogueGain": 6.0})
-        #self.picam2.set_controls({"ExposureTime": 1000000}) # for blurring - but can get over exposed at night
-        #self.picam2.set_controls({"FrameDurationLimits": (1000,1000)})
-        #self.picam2.set_controls({"ExposureTime": 100000000, "AnalogueGain": 1.0})
-        #self.picam2.video_configuration.controls.FrameRate = 90
+        # _config = self.picam2.create_video_configuration(
+        #             main={"size": res_xy, "format": "RGB888"},
+        #             controls={ "FrameDurationLimits": (22222, 33333)}, # ex FrameDurationLimits:  24fps = 1000000/24 = 41667
+        #             buffer_count=1)#, controls={"FrameDurationLimits": (22222, 33333)})
+        # self.picam2.configure(_config)
+        # #  set_controls must come after config!!
+        # self.picam2.set_controls({"AwbEnable": 0})
+        # #self.picam2.set_controls({"AeEnable": 0})
+        # self.picam2.set_controls({"AeMeteringMode": controls.AeMeteringModeEnum.Spot})
+        # self.picam2.set_controls({"AnalogueGain": 6.0})
+        # #self.picam2.set_controls({"ExposureTime": 1000000}) # for blurring - but can get over exposed at night
+        # #self.picam2.set_controls({"FrameDurationLimits": (1000,1000)})
+        # #self.picam2.set_controls({"ExposureTime": 100000000, "AnalogueGain": 1.0})
+        # #self.picam2.video_configuration.controls.FrameRate = 90
         self.picam2.start()
         time.sleep(0.2)
 
