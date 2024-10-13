42f2f631a04ad56e5526a7197f676cdd626bd293
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-10-13 18:10:46 +0100

    plop

--- Source/lumotag/rasp_hardware_common.py
+++ Source/lumotag/rasp_hardware_common.py
@@ -221,7 +221,7 @@ class CsiCameraImageGenRCAMv2NOIR(factory.ImageGenerator):
         self.picam2.configure(_config)
         #  set_controls must come after config!!
         #self.picam2.set_controls({"AwbEnable": 0})
-        self.picam2.set_controls({"AeMeteringMode": controls.AeMeteringModeEnum.Spot})
+        self.picam2.set_controls({"AwbEnable": 0, "AeMeteringMode": controls.AeMeteringModeEnum.Spot})
         #self.picam2.set_controls({"AnalogueGain": 5.0})
         self.picam2.start()
         time.sleep(0.2)
