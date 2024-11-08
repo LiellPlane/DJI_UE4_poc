153b78adf626b469ff33285f3ab95df7e8c43e85
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2024-11-08 19:13:20 +0000

    plop

--- Source/lumotag/setup/run_lumo.py
+++ Source/lumotag/setup/run_lumo.py
@@ -9,6 +9,8 @@ import urllib.request
 import time
 import random
 
+skip_update = False
+
 def check_internet_connection():
     websites = [
         "http://www.google.com",
@@ -26,9 +28,11 @@ def check_internet_connection():
             continue
     return False
 
-while not check_internet_connection():
-    print("No internet connection. Retrying in 5 seconds...")
-    time.sleep(5)
+
+if skip_update is True:
+    while not check_internet_connection():
+        print("No internet connection. Retrying in 5 seconds...")
+        time.sleep(5)
 
 
 
@@ -44,57 +48,27 @@ print(f"MY_ID: {MY_ID} url: {url}")
 #repo = 'https://github.com/LiellPlane/DJI_UE4_poc.git'
 #codepath = "/home/scambilight/DJI_UE4_poc"
 # **** might need sudo git config --global --add safe.directory /home/scambilight/DJI_UE4_poc
-try:
-    # Get the current date and time
-    now = datetime.now()
-    printabletime = now.strftime("%Y-%m-%d_%H-%M-%S")
-    if os.path.exists(codepath):
-        fetch_result = subprocess.run(['sudo', 'git', 'pull', '--ff-only'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(fetch_result)
-        with open('/home/lumotag/retardedlinuxpull.cunt', 'w') as file:
-            file.write(f"{printabletime}{str(fetch_result)}")
 
-    else:
-        with open('/home/lumotag/retardedlinuxclone.cunt', 'w') as file:
-            file.write(f"{printabletime}trying to clone this cunt of a thing")
-        fetch_result = subprocess.run(['sudo', 'git', 'clone', repo], cwd="/home/lumotag/", text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        with open('/home/lumotag/retardedlinuxclone.cunt', 'w') as file:
-            file.write(f"{printabletime}{str(fetch_result)}")
-except Exception as e:
-    with open('/home/lumotag/retardedlinuxfailedclone.cunt', 'w') as file:
-        file.write(f"{printabletime}linux retarded cunt failure: {e}")
+if skip_update is True:
+    try:
+        # Get the current date and time
+        now = datetime.now()
+        printabletime = now.strftime("%Y-%m-%d_%H-%M-%S")
+        if os.path.exists(codepath):
+            fetch_result = subprocess.run(['sudo', 'git', 'pull', '--ff-only'], cwd=codepath, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(fetch_result)
+            with open('/home/lumotag/retardedlinuxpull.cunt', 'w') as file:
+                file.write(f"{printabletime}{str(fetch_result)}")
 
-# commands = [
-#     'cd',
-#     '/home/lumotag/DJI_UE4_poc/',
-#     '&&',
-#     'git',
-#     'fetch',
-#     '&&',
-#     'git',
-#     'pull',
-#     '&&',
-#     'cd',
-#     '/home/lumotag/'
-# ]
-
-
-# if os.path.exists(codepath):
-#     fetch_result = subprocess.run(commands, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     print(fetch_result)
-# else:
-#     print(
-#         "trying to clone repo from web - this is incorrect state- should exist already")
-#     subprocess.run(['git', 'clone', repo])
-
-# the speaker does not like being SUDO 
-# original_uid = os.getenv('SUDO_UID')
-# original_gid = os.getenv('SUDO_GID')
-# os.setgid(int(original_gid))
-# # Change the user ID
-# os.setuid(int(original_uid))
-
-
+        else:
+            with open('/home/lumotag/retardedlinuxclone.cunt', 'w') as file:
+                file.write(f"{printabletime}trying to clone this cunt of a thing")
+            fetch_result = subprocess.run(['sudo', 'git', 'clone', repo], cwd="/home/lumotag/", text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            with open('/home/lumotag/retardedlinuxclone.cunt', 'w') as file:
+                file.write(f"{printabletime}{str(fetch_result)}")
+    except Exception as e:
+        with open('/home/lumotag/retardedlinuxfailedclone.cunt', 'w') as file:
+            file.write(f"{printabletime}linux retarded cunt failure: {e}")
 
 
 
