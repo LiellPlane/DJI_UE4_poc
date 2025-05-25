8a160c04e3da5f39afb745fe5d75031aa467cb1c
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2025-05-25 18:19:07 +0100

    Plop

--- account_gen/bot.py
+++ account_gen/bot.py
@@ -2,7 +2,7 @@ from interactions import WebInteraction, FontConfig
 from vision import generate_string_pattern,display_image
 def main():
     bot = WebInteraction(url="https://old.reddit.com")
-    calib_img, calib_str = bot.get_calibration_screenshot(url="https://old.reddit.com",save_to_disk=True)
+    calib_img, calib_str = bot.get_calibration_screenshot(url="https://old.reddit.com",save_to_disk=False)
     str_pattern = generate_string_pattern("CALIBERT", FontConfig)
     display_image(str_pattern,wait_for_key=True)
 
