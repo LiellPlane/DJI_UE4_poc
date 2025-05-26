90155a4ff85b4964b76b04af39992bc282f84264
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2025-05-26 21:18:23 +0100

    plop

--- account_gen/bot.py
+++ account_gen/bot.py
@@ -1,10 +1,12 @@
 from interactions import WebInteraction, FontConfig
 from vision import generate_string_pattern,display_image, find_searchpattern_scale, PatternMatch
 def main():
-    bot = WebInteraction(url="https://gtmetrix.com/")
+    bot = WebInteraction(url="https://old.reddit.com")
     # calib_img, calib_str = bot.get_calibration_screenshot(url="https://old.reddit.com",save_to_disk=False)
     # str_pattern = generate_string_pattern("CALIBERT", FontConfig)
-    text_img = bot.get_text_area_screenshot(url="https://gtmetrix.com/",text="COOKIE")
+
+    # calibration stage
+    text_img = bot.get_text_area_screenshot(url="https://old.reddit.com",text="COOKIE")
     screenshot, scale = bot.get_raw_screenshot()
     print(f"scale: {scale}")
     # display_image(text_img,wait_for_key=True)
