0f68851ebae7c552e34083dad581f26990159dde
LiellPlane <106975411+LiellPlane@users.noreply.github.com>
2025-12-17 17:50:30 +0000

    plop

--- Source/lumotag/lumogun.py
+++ Source/lumotag/lumogun.py
@@ -398,13 +398,12 @@ def main():
                 # if players["me"].get_torch_energy() < 5:
                 #    is_torch_reqd = False
 
-                
-                set_laser(state=is_torch_reqd)
-      
+                set_torch(state=is_torch_reqd)
+
                 if deactivate_lser is True:
-                    set_torch(state=is_torch_reqd)
+                    set_laser(state=is_torch_reqd)
                 else:
-                    set_torch(state=False)
+                    set_laser(state=False)
 
                 result_zoom = zoom_debounce.trigger_1shot_simple_High(is_zoom_reqd)
 
